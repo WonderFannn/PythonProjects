81dca556dc9b4dad8d967ad48de9fd9e6ec76ec6
汪帆 <sail032@live.cn>
2017-09-04 10:10:49 +0800

    添加ebo停车优惠脚本

---
+++ eboparking.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import time
+import webbrowser
+webbrowser.open('http://wx.ebopark.com/index.php/Home/Dedu/offset?pc=510107027&id=2&openid=oEe3kwXkYnEP8j9Al6I0L18_hSZU&timestamp='+str(int(time.time())*1000)+'&uid=2')
