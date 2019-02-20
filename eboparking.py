4afe01c2e5d72ed234220a6c9f6bfb4fca2c0df5
汪帆 <sail032@live.cn>
2019-02-20 09:13:03 +0800

    更新

--- eboparking.py
+++ eboparking.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import time
 import webbrowser
-# webbrowser.open('http://wx.ebopark.com/index.php/Home/Dedu/offset?pc=510107027&id=2&openid=oEe3kwXkYnEP8j9Al6I0L18_hSZU&timestamp='+str(int(time.time())*1000)+'&uid=2')
+webbrowser.open('http://wx.ebopark.com/index.php/Home/Dedu/offset?pc=510107027&id=2&openid=oEe3kwXkYnEP8j9Al6I0L18_hSZU&timestamp='+str(int(time.time())*1000)+'&uid=2')
 webbrowser.open('http://wx.ebopark.com/index.php/Home/Discount/dedu_2-510107027-25-'+str(int(time.time())*1000)+'-4')
--- getUrl.py
+++ getUrl.py
@@ -4,12 +4,42 @@ import urllib2
 import webbrowser
 import re
 text = 'H停车券'
-for num in range(619500,619999):
+text2 = '高新天虹'
+text3 = '成都'
+text4 = '高新区'
+text5 = '奥克斯'
+fileName = "getNum.txt"
+f = open(fileName)
+num = 620365
+while 1:
+    line = f.readline()
+    if not line:
+        break
+    num = int(line)
+f.close()
+flag = 0
+while 1:
 	url = 'https://m.honglingjin.cn/user/coupon/info/'+str(num)+'_0.html?coupon_source=offline'
-	response = urllib2.urlopen(url)
-	html = response.read()
-	m = re.search("<title>.*</title>", html)
-	# print m.group() # 这里输出结果 <title>Apple</title>
-	print str(num)+' : '+m.group().strip("</title>") #问题应该出现在这个正则
+	try:
+		pass
+		response = urllib2.urlopen(url)
+		html = response.read()
+		m = re.search("<title>.*</title>", html)
+		title = m.group().strip("</title>")
+	except Exception as e:
+		continue
+	print str(num)+' : '+title
+	if title:
+		flag = 0
+	else:
+		flag += 1
 	if text in m.group():
 		webbrowser.open(url)
+	if text2 in html or text3 in html or text4 in html or text5 in html:
+		webbrowser.open(url)
+	if flag == 30:
+		f = open(fileName, "w")
+		print >> f, "%d" % (num - 29)
+		f.close()
+		break
+	num += 1
